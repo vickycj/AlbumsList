3c119692222c5b4d998c5ec0c41a663dde16e038
vickycj <vygneshwaran@gmail.com>
2019-05-09 23:53:33 +0530

    added room dao

--- app/src/main/java/com/vicky/apps/datapoints/base/AppConstants.kt
+++ app/src/main/java/com/vicky/apps/datapoints/base/AppConstants.kt
@@ -5,4 +5,6 @@ object AppConstants {
 
     const val TABLE_NAME = "albums_table"
 
+    const val COLUMN_TITLE = "title"
+
 }
---
+++ app/src/main/java/com/vicky/apps/datapoints/data/room/dao/AlbumsDao.kt
@@ -0,0 +1,23 @@
+package com.vicky.apps.datapoints.data.room.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.vicky.apps.datapoints.base.AppConstants
+import com.vicky.apps.datapoints.data.room.entity.Albums
+
+
+@Dao
+interface AlbumsDao {
+
+    @Query("SELECT * from "+AppConstants.TABLE_NAME+" ORDER BY "+AppConstants.COLUMN_TITLE+" ASC")
+    fun getAllAlbums(): LiveData<List<Albums>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(albums: List<Albums>)
+
+    @Query("DELETE FROM "+AppConstants.TABLE_NAME+ "")
+    fun deleteAll()
+}
